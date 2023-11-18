2e9c6fe5d92e6b5b76edbd24571f6efa9095162e
bso112 <bso11246@gmail.com>
2023-11-18 17:42:01 +0900

    구글 서비스 플러그인 삭제 (json 없어서 jitpack 빌드 오류)

--- app/src/main/java/com/manta/snslogindemo/MainActivity.kt
+++ app/src/main/java/com/manta/snslogindemo/MainActivity.kt
@@ -8,22 +8,22 @@ import com.manta.snslogin.google.registerForGoogleLoginResult
 
 class MainActivity : AppCompatActivity() {
 
-    private val googleLoginLauncher = registerForGoogleLoginResult(
-        onSuccess = {
-            Toast.makeText(this, "success $it", Toast.LENGTH_SHORT).show()
-            Log.d("mylog", "success: $it")
-        },
-        onFailure = {
-            Toast.makeText(this, "failure $it", Toast.LENGTH_SHORT).show()
-            Log.d("mylog", "failure: $it")
-        },
-    )
+//    private val googleLoginLauncher = registerForGoogleLoginResult(
+//        onSuccess = {
+//            Toast.makeText(this, "success $it", Toast.LENGTH_SHORT).show()
+//            Log.d("mylog", "success: $it")
+//        },
+//        onFailure = {
+//            Toast.makeText(this, "failure $it", Toast.LENGTH_SHORT).show()
+//            Log.d("mylog", "failure: $it")
+//        },
+//    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        googleLoginLauncher.launch(getString(R.string.default_web_client_id))
+//        googleLoginLauncher.launch(getString(R.string.default_web_client_id))
     }
 
 
